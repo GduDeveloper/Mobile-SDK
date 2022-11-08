1c8b1cfd333c9323e420bba3fbfbacb61e7c8528
zzl <zzl@gdu-tech.com>
2022-11-08 11:26:37 +0800

    修复虚拟摇杆相关bugs

--- app/src/main/java/com/gdu/demo/VirtualStickActivity.java
+++ app/src/main/java/com/gdu/demo/VirtualStickActivity.java
@@ -1,15 +1,27 @@
 package com.gdu.demo;
 
 import android.app.Activity;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.CameraUpdateFactory;
+import com.amap.api.maps.CoordinateConverter;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.model.BitmapDescriptorFactory;
+import com.amap.api.maps.model.LatLng;
+import com.amap.api.maps.model.Marker;
+import com.amap.api.maps.model.MarkerOptions;
 import com.gdu.common.error.GDUError;
 import com.gdu.demo.views.JoystickView;
 import com.gdu.demo.views.OnJoystickListener;
+import com.gdu.drone.LocationCoordinate3D;
 import com.gdu.flightcontroller.FlightControlData;
 import com.gdu.sdk.flightcontroller.FlightControllerState;
 import com.gdu.sdk.flightcontroller.GDUFlightController;
@@ -24,10 +36,17 @@ public class VirtualStickActivity extends Activity {
     private JoystickView mLeftJoystickView;
     private JoystickView mRightJoystickView;
     private TextView mFCInfoTextView;
+    private MapView mMapView;
+    private Context mContext;
 
     private GDUFlightController mFlightController;
     private Timer sendVirtualStickDataTimer;
     private SendVirtualStickDataTask sendVirtualStickDataTask;
+
+    private AMap aMap;
+    private Marker mPlaneMarker;
+    private CoordinateConverter coordinateConverter;
+    private MarkerOptions mPlaneMarkerOptions;
 
     private float pitch;
     private float roll;
@@ -38,16 +57,49 @@ public class VirtualStickActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_virtual_stick);
+        mContext = this;
         initView();
+        initMap(savedInstanceState);
         initData();
     }
+
+    private void initMap(Bundle savedInstanceState) {
+        mMapView.onCreate(savedInstanceState);
+        aMap = mMapView.getMap();
+        aMap.setMapType(AMap.MAP_TYPE_SATELLITE);
+        coordinateConverter = new CoordinateConverter(this);
+        coordinateConverter.from(CoordinateConverter.CoordType.GPS);
+    }
+
 
     private void initData() {
         mFlightController = SdkDemoApplication.getAircraftInstance().getFlightController();
         mFlightController.setStateCallback(new FlightControllerState.Callback() {
             @Override
             public void onUpdate(FlightControllerState flightControllerState) {
-                showText(mFCInfoTextView, flightControllerState.getString());
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        LocationCoordinate3D locationCoordinate3D  = flightControllerState.getAircraftLocation();
+                        LatLng latLng = new LatLng(locationCoordinate3D.getLatitude(), locationCoordinate3D.getLongitude());
+                        if (mPlaneMarker != null) {
+                            coordinateConverter.coord(latLng);
+                            mPlaneMarker.setPosition(coordinateConverter.convert());
+                            mPlaneMarker.setRotateAngle(-(float) flightControllerState.getAttitude().yaw);
+                        } else {
+                            mPlaneMarkerOptions = new MarkerOptions();
+                            mPlaneMarkerOptions.position(latLng);
+                            Bitmap bitmap = BitmapFactory.decodeResource(mContext.getResources(),
+                                    R.mipmap.icon_plane).copy(Bitmap.Config.ARGB_8888, true);
+                            mPlaneMarkerOptions.anchor(0.5f, 0.5f);
+                            mPlaneMarkerOptions.icon(BitmapDescriptorFactory.fromBitmap(bitmap));
+                            mPlaneMarker = aMap.addMarker(mPlaneMarkerOptions);
+                            mPlaneMarker.setRotateAngle(-(float) flightControllerState.getAttitude().yaw);
+                            aMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 16));
+                        }
+                        mFCInfoTextView.setText(flightControllerState.getString());
+                    }
+                });
             }
         });
         mFlightController.setVirtualStickModeEnabled(true, new CommonCallbacks.CompletionCallback() {
@@ -59,6 +111,7 @@ public class VirtualStickActivity extends Activity {
     }
 
     private void initView() {
+        mMapView = findViewById(R.id.map);
         mFCInfoTextView = findViewById(R.id.tv_show_msg);
         mLeftJoystickView = findViewById(R.id.joystick_view_left);
         mRightJoystickView = findViewById(R.id.joystick_view_right);
@@ -86,8 +139,8 @@ public class VirtualStickActivity extends Activity {
 //                    }
 //                }
 
-                pitch = (float) (pitchJoyControlMaxSpeed * pX);
-                roll = (float) (rollJoyControlMaxSpeed * pY);
+                pitch = (float) (pitchJoyControlMaxSpeed * pY);
+                roll = (float) (rollJoyControlMaxSpeed * pX);
 
                 if (null == sendVirtualStickDataTimer) {
                     sendVirtualStickDataTask = new SendVirtualStickDataTask();
