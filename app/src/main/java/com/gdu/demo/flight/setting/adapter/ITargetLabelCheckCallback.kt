9df7427b3aa7aa235c332714310a34f6b485fec6
lixiqiang <842702016@qq.com>
2025-02-11 15:32:23 +0800

    修改RTK设置相关页面和接口

--- app/src/main/java/com/gdu/demo/flight/setting/adapter/ITargetLabelCheckCallback.kt
+++
@@ -1,8 +0,0 @@
-package com.gdu.demo.flight.setting.adapter
-
-import com.gdu.demo.flight.setting.bean.TargetDetectModel
-
-
-interface ITargetLabelCheckCallback {
-    fun onCheckChange(data: TargetDetectModel?)
-}
