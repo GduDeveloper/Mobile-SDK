5cd6a5d08545a010e86023e5082ae81e5d39dd69
WIN-SEN2IGF5TLI\Administrator <1032167624@qq.com>
2026-08-21 11:48:37 +0800

    deovpt:提交云台渲染

--- app/src/main/java/com/gdu/demo/CameraGimbalActivity.java
+++ app/src/main/java/com/gdu/demo/CameraGimbalActivity.java
@@ -31,7 +31,9 @@ import com.gdu.sdk.camera.SystemState;
 //import com.gdu.sdk.camera.VideoFeeder;
 //import com.gdu.sdk.codec.GDUCodecManager;
 //import com.gdu.sdk.codec.ImageProcessingManager;
+import com.gdu.sdk.codec.CodecManager;
 import com.gdu.sdk.gimbal.Gimbal;
+import com.gdu.sdk.manager.SDKManager;
 import com.gdu.sdk.products.Aircraft;
 import com.gdu.sdk.util.CommonCallbacks;
 import com.gdu.sdk.util.FileSaveUtil;
@@ -50,6 +52,8 @@ public class CameraGimbalActivity extends Activity implements TextureView.Surfac
     private final String OUTPATH = Environment.getExternalStorageDirectory() + "/gdu/sdk/local/";//本地副本的保存路径
 //    private VideoFeeder.VideoDataListener videoDataListener = null;
 //    private GDUCodecManager codecManager = null;
+
+    private CodecManager mCodecManager;
 
     private TextureView mGduPlayView;
     private TextView mInfoTextView;
@@ -73,6 +77,13 @@ public class CameraGimbalActivity extends Activity implements TextureView.Surfac
         super.onCreate(savedInstanceState);
         mContext = this;
         setContentView(R.layout.activity_camera_gimbal);
+
+        // 初始化CodecManager
+        Aircraft aircraft = (Aircraft) SDKManager.getInstance().getProduct();
+        if (aircraft != null) {
+            mCodecManager = aircraft.getCodecManager();
+        }
+
         initView();
         initData();
         initListener();
@@ -599,6 +610,9 @@ public class CameraGimbalActivity extends Activity implements TextureView.Surfac
 
     @Override
     public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        if (mCodecManager != null) {
+            mCodecManager.startPreview(0, surface);
+        }
 //        if (codecManager == null) {
 //            codecManager = new GDUCodecManager(mContext, mGduPlayView, width, height);
 //        }
@@ -606,11 +620,16 @@ public class CameraGimbalActivity extends Activity implements TextureView.Surfac
 
     @Override
     public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
-
+        if (mCodecManager != null) {
+            mCodecManager.onSurfaceLayoutChanged(0, width, height);
+        }
     }
 
     @Override
     public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        if (mCodecManager != null) {
+            mCodecManager.stopPreview(0);
+        }
         return false;
     }
 
